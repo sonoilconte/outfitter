0076af5f84943d0e0d7d139c70eecf5a8c0dfc3a
sonoilconte <shammersinger@yahoo.com>
2017-10-17 18:21:37 -0700

    removed all ES6 making heroku mad

--- app/assets/javascripts/image-preview.js
+++ app/assets/javascripts/image-preview.js
@@ -1,10 +1,10 @@
-$(document).ready( () => {
-  $(".image-input").on("change", (e) => {
+$(document).ready( function(){
+  $(".image-input").on("change", function(e){
     console.log("image file detected!");
     let files = e.target.files;
     let image = files[0];
     let reader = new FileReader();
-    reader.onload = (file) => {
+    reader.onload = function(file){
       let img = new Image();
       img.src = file.target.result;
       $(".image-preview").html(img);
