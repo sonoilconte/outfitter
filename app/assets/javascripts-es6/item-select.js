26d61c60e4c794fe09cfcb69b4291abeb5683e86
sonoilconte <shammersinger@yahoo.com>
2017-11-02 11:54:59 -0700

    forEach, moved itemSelect

--- app/assets/javascripts-es6/item-select.js
+++ app/assets/javascripts-es6/item-select.js
@@ -1,37 +1,36 @@
 $(document).ready(() => {
-  const items = document.getElementsByClassName("item");
-  for(let i=0; i<items.length; i++){
-    items[i].addEventListener("click", onItemSelect);
-  }
-});
 
-const onItemSelect = (e) => {
-  e.preventDefault();
-  document.getElementById("board-advice").innerHTML = "";
-  const itemNode = e.target.closest(".item");
-  const itemId = itemNode.getAttribute("data-item-id");
-  const section = itemNode.getAttribute("data-section-id");
-  const imgUrl = itemNode.getAttribute("data-large-img");
-  let imgNode = document.createElement("img");
-  imgNode.src = imgUrl;
+  const onItemSelect = (e) => {
+    e.preventDefault();
+    document.getElementById("board-advice").innerHTML = "";
+    const itemNode = e.target.closest(".item");
+    const itemId = itemNode.getAttribute("data-item-id");
+    const section = itemNode.getAttribute("data-section-id");
+    const imgUrl = itemNode.getAttribute("data-large-img");
+    const imgNode = document.createElement("img");
+    imgNode.src = imgUrl;
 
-  const sectionMap = {
-    shirt: {
-      imgContainerId: "shirt-target",
-      inputName: "shirt_id"
-    },
-    pants: {
-      imgContainerId: "pants-target",
-      inputName: "pants_id"
-    },
-    shoes: {
-      imgContainerId: "shoes-target",
-      inputName: "shoes_id"
-    }
+    const sectionMap = {
+      shirt: {
+        imgContainerId: "shirt-target",
+        inputName: "shirt_id"
+      },
+      pants: {
+        imgContainerId: "pants-target",
+        inputName: "pants_id"
+      },
+      shoes: {
+        imgContainerId: "shoes-target",
+        inputName: "shoes_id"
+      }
+    };
+    const sectionIds = sectionMap[section];
+    document.getElementsByName(sectionIds.inputName)[0].setAttribute("value", itemId);
+    const sectionNode = document.getElementById(sectionIds.imgContainerId);
+    sectionNode.innerHTML = "";
+    sectionNode.appendChild(imgNode);
   };
-  const sectionIds = sectionMap[section];
-  document.getElementsByName(sectionIds.inputName)[0].setAttribute("value", itemId);
-  let sectionNode = document.getElementById(sectionIds.imgContainerId);
-  sectionNode.innerHTML = "";
-  sectionNode.appendChild(imgNode);
-};
+
+  const items = document.getElementsByClassName("item");
+  [].forEach.call(items, item => item.addEventListener("click", onItemSelect));
+});
