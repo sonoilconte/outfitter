2a802c63a5dadfd76e7eadea28fa791ec334aee1
sonoilconte <shammersinger@yahoo.com>
2017-11-01 17:54:43 -0700

    es-6 for item select

--- app/assets/javascripts/item-select.js
+++ app/assets/javascripts/item-select.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
-  var items = document.getElementsByClassName("item");
-  for(var i=0; i<items.length; i++){
+$(document).ready(() => {
+  const items = document.getElementsByClassName("item");
+  for(let i=0; i<items.length; i++){
     items[i].addEventListener("click", onItemSelect);
   }
 });
 
-var onItemSelect = function(e){
+const onItemSelect = (e) => {
   e.preventDefault();
   document.getElementById("board-advice").innerHTML = "";
-  var itemNode = this.closest(".item");
-  var itemId = itemNode.getAttribute("data-item-id");
-  var section = itemNode.getAttribute("data-section-id");
-  var imgUrl = itemNode.getAttribute("data-large-img");
-  var imgNode = document.createElement("img");
+  const itemNode = e.target.closest(".item");
+  const itemId = itemNode.getAttribute("data-item-id");
+  const section = itemNode.getAttribute("data-section-id");
+  const imgUrl = itemNode.getAttribute("data-large-img");
+  let imgNode = document.createElement("img");
   imgNode.src = imgUrl;
 
-  var sectionMap = {
+  const sectionMap = {
     shirt: {
       imgContainerId: "shirt-target",
       inputName: "shirt_id"
@@ -29,10 +29,9 @@ var onItemSelect = function(e){
       inputName: "shoes_id"
     }
   };
-
-  var sectionIds = sectionMap[section];
+  const sectionIds = sectionMap[section];
   document.getElementsByName(sectionIds.inputName)[0].setAttribute("value", itemId);
-  var sectionNode = document.getElementById(sectionIds.imgContainerId);
+  let sectionNode = document.getElementById(sectionIds.imgContainerId);
   sectionNode.innerHTML = "";
   sectionNode.appendChild(imgNode);
 };
