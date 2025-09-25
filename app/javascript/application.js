bb3196724a2bbd9b5a13242e673d97d0f78ba4d7
Sepp Hammer <sepp.r.hammer@gmail.com>
2025-09-25 15:14:36 -0700

    Resolve JS mapping issue

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./custom/itemSelect"
-import "./custom/imagePreview"
-
+import "custom/itemSelect"
+import "custom/imagePreview"
